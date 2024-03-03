b9b5c1d00f0c45ff04d78b6bc646a91d32012ef8
Yuriy Pilipenko <yurik2061@gmail.com>
2024-03-03 16:00:44 +0200

    temp

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -25,8 +25,6 @@ export const ValidationSchema = (totalBalance) => z.object({
         .min(3, 'Date must be 3 or more characters long')  ,
     description: z
         .string()
-        .trim()
-        .min(4) 
         .refine((val) => !val.toLowerCase().startsWith('qwe'), {
             message: 'Enter a different description'
           }) ,      
