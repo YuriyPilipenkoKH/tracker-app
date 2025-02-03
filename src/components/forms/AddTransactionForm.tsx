b7b1241b1c2a34c457749d9471de6b25d4ff7756
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-03 16:42:38 +0200

    eslint

--- src/components/forms/AddTransactionForm.tsx
+++ src/components/forms/AddTransactionForm.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Transaction, TransactionSchema } from '../../models/transaction'
+import { zodResolver } from '@hookform/resolvers/zod'
+
 
 const AddTransactionForm = () => {
+  const {
+    register, 
+    // handleSubmit,
+    // formState,
+    // reset,
+  } = useForm<Transaction >({
+    defaultValues: {  
+      name: '',
+      price: 0 ,
+      dateTime: undefined,
+      description: ''
+       },
+        mode:'all',
+        resolver: zodResolver(TransactionSchema), })
   return (
-    <div>AddTransactionForm</div>
+    <form >
+
+    </form>
   )
 }
 
