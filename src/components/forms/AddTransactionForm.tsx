519d0c8c4baf9bfe0cfc895b90c32dee649bbc1e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-03 16:54:58 +0200

    temp

--- src/components/forms/AddTransactionForm.tsx
+++ src/components/forms/AddTransactionForm.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { Transaction, TransactionSchema } from '../../models/transaction'
 import { zodResolver } from '@hookform/resolvers/zod'
 
-
+const totalBalance = 1000
 const AddTransactionForm = () => {
   const {
     register, 
-    // handleSubmit,
-    // formState,
-    // reset,
+    handleSubmit,
+    formState,
+    reset,
   } = useForm<Transaction >({
     defaultValues: {  
       name: '',
@@ -18,7 +18,22 @@ const AddTransactionForm = () => {
       description: ''
        },
         mode:'all',
-        resolver: zodResolver(TransactionSchema), })
+        resolver: zodResolver(TransactionSchema(totalBalance)), })
+    const {
+      errors,
+      isDirty,
+      isValid ,
+      isSubmitting,
+      isSubmitSuccessful,
+      isLoading 
+    } = formState
+
+    useEffect(() => {
+      if(isSubmitSuccessful) {
+          reset()
+      }
+      }, [isSubmitSuccessful, reset])
+      
   return (
     <form >
 
---
+++ src/pages/Temp.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import { getTransactions } from '../lib/axios'
+
+const Temp = () => {
+  const [transactions, setTransactions] = useState([])
+  const [reRender, setReRender] = useState(false)
+
+  useEffect(() => {
+    getTransactions().then(list => {
+      setTransactions(list)
+    })
+  }, [reRender])
+  
+  const totalBalance = transactions.reduce((acc, transaction) => {
+    return acc + (transaction.price || 0);
+  }, 0);
+
+
+
+
+
+
+
+  
+  //  function addNewTransaction (e) {
+//     e.preventDefault()
+//     const url = process.env.REACT_APP_API_URL+'/transaction'
+//     fetch(url, {
+//       method: 'POST',
+//       headers: {'Content-type': 'application/json' },
+//       body: JSON.stringify({ name,description,dateTime})
+//     }).then(response => {
+//       response.json()
+//       .then(data => {
+//       console.log(data)
+//       })
+//     })
+//   }
+const { name, price, description, dateTime } = getValues();
+
+function addNewTransaction(data) {
+  // e.preventDefault();
+  const url = process.env.REACT_APP_API_URL + '/transaction';
+  console.log(url)
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({ name, price, description, dateTime }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      setReRender(true)
+
+      console.log(data);
+    })
+    .finally(setReRender(false))
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+  return (
+    <div>Temp</div>
+  )
+}
+
+export default Temp
