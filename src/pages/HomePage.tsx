f8e78ef371b5f4ba400bdf6a48bf6633812e76a9
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-03 15:46:39 +0200

    root

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,12 +4,9 @@ const HomePage = () => {
 
   return (
     <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-4">
-        <div className="bg-slate-200 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
         HomePage
 
-        </div>
-      </div>
+
     </div>
   )
 }
