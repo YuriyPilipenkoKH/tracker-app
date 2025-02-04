0646657ad7d84f5679673eeb2bd24e6c545d422f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-04 07:44:33 +0200

    google signin button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,15 @@
+import SignInButton from "../components/button/SignInButton"
 import { useAuthStore } from "../store/useAuthStore"
 
 
 const HomePage = () => {
 const {authUser} = useAuthStore()
+console.log('authUser',authUser);
   return (
     <div className="h-screen bg-base-200">
         HomePage
 
-
+      <SignInButton/>
     </div>
   )
 }
