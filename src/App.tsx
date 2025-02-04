ec30610678982ee3078d7643ceeeabd34b6e83e3
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-04 07:09:28 +0200

    Add toast notifications and enhance auth store with login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import { Toaster } from 'react-hot-toast';
+import { options } from "./lib/hotToast"
 
 function App() {
  
@@ -10,6 +12,10 @@ function App() {
       <Routes>
       <Route path="/" element ={<HomePage/>}/>
       </Routes>
+
+      <Toaster
+       position="top-center" 
+      toastOptions={options} />
     
     </div>
   )
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,6 +1,8 @@
 import {create} from 'zustand'
 import { User } from '../types/userTypes';
 import { axios } from '../lib/axios';
+import { AxiosError } from 'axios';
+import toast from 'react-hot-toast';
 
 const HOST = import.meta.env.VITE_HOST;
 const BASE_URL = HOST;
@@ -8,12 +10,15 @@ const BASE_URL = HOST;
 interface AuthStoreTypes {
   authUser: User | null 
   isCheckingAuth: boolean
+  isLoggingIn: boolean,
+  logIn: () => Promise<boolean | undefined>
   checkAuth: () => Promise<void>
 }
 
 export const useAuthStore = create<AuthStoreTypes>((set,get) => ({
   authUser: null,
   isCheckingAuth: true,
+  isLoggingIn: false,
   checkAuth: async() =>{
     try {
       const response = await axios.get('/auth/check')
@@ -26,6 +31,19 @@ export const useAuthStore = create<AuthStoreTypes>((set,get) => ({
     }
     finally{
       set({isCheckingAuth: false})
+    }
+  },
+  logIn : async () => {
+    try {
+      console.log();
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        toast.error(error.response.data.message);
+      }
+      return false
+    }
+    finally{
+      set({isLoggingIn: false})
     }
   },
 }))
