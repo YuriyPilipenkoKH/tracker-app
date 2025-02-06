510795c5bf32f102943b0a6271d0a0087f7e3ea9
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-06 10:26:51 +0200

    styling profile form

--- src/components/forms/ProfileForm.tsx
+++ src/components/forms/ProfileForm.tsx
@@ -5,7 +5,7 @@ import {  Mail, User } from "lucide-react";
 const ProfileForm = () => {
   const { authUser, } = useAuthStore()
   return (
-    <div className="space-y-6">
+    <div className="space-y-6 ">
     <div className="space-y-1.5">
       <div className="text-sm text-zinc-400 flex items-center gap-2">
         <User className="w-4 h-4" />
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -9,14 +9,13 @@ const {authUser} = useAuthStore()
 console.log('authUser',authUser);
 
   return (
-     <div className="grid pt-20 transition-all duration-800 ease-in-out">
-      <div className="max-w-2xl mx-auto p-4 py-8">
+     <div className="grid pt-5 transition-all duration-800 ease-in-out">
+      <div className=" mx-auto p-4 py-8 w-[320px] md:w-[500px]">
         <div className="bg-base-300 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
           </div>
-          {/* <AvatarUploadForm /> */}
           <AvatarUploadFormCustom />
           <ProfileForm  />
           <AccountInfo/>
