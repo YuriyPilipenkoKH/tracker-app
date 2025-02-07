53be1b5a8c42c460478fab3fa0c16636746a42ac
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-07 18:40:51 +0200

    rm extra markup

--- src/components/forms/ProfileForm.tsx
+++ src/components/forms/ProfileForm.tsx
@@ -43,21 +43,6 @@ const ProfileForm = () => {
       }
   return (
     <div className="space-y-6 ">
-    {/* <div className="space-y-1.5">
-      <div className="text-sm text-zinc-400 flex items-center gap-2">
-        <User className="w-4 h-4" />
-        Full Name
-      </div>
-      <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.name}</p>
-    </div>
-
-    <div className="space-y-1.5">
-      <div className="text-sm text-zinc-400 flex items-center gap-2">
-        <Mail className="w-4 h-4" />
-        Email Address
-      </div>
-      <p className="px-4 py-2.5 bg-base-200 rounded-lg border">{authUser?.email}</p>
-    </div> */}
 
     <form onSubmit={handleSubmit(onSubmit)}
         className='relative flex flex-col gap-3 w-full py-5'
