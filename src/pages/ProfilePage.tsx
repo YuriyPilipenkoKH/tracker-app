20c01c02d9ef0ac2735461ae4762bb7830f2fceb
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-07 20:37:42 +0200

    refactor profile components for improved layout and styling

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -11,7 +11,7 @@ const ProfilePage = () => {
   return (
      <div className="grid pt-5 transition-all duration-800 ease-in-out">
       <div className=" mx-auto p-4 py-8 w-[320px] md:w-[500px]">
-        <div className="bg-base-300 rounded-xl p-6 space-y-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-6">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
