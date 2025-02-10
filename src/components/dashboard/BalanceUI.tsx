4fdea6fb934b9502bf2f03d4c2f98f157836ac6d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-10 14:26:49 +0200

    BalanceUI completed

--- src/components/dashboard/BalanceUI.tsx
+++ src/components/dashboard/BalanceUI.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useFinanceStore } from '../../store/useFinanceStore'
 
 const BalanceUI = () => {
+  const {totalBalance} = useFinanceStore()
   return (
-    <div>BalanceUI</div>
+    <div>
+      <span className='flex items-center jusify-center p-5 bordered bg-[var(--vivid-green)] text-xl font-bold'>
+        {totalBalance}
+      </span>
+    </div>
   )
 }
 
