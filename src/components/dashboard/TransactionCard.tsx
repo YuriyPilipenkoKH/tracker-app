a0062475b2f0b1788abc319027bb0bb4aaa744fd
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-10 10:54:33 +0200

    style: enhance TransactionCard layout for improved readability and presentation

--- src/components/dashboard/TransactionCard.tsx
+++ src/components/dashboard/TransactionCard.tsx
@@ -14,14 +14,27 @@ const TransactionCard:React.FC<TransactionCardProps> = ({transaction}) => {
     description
   } = transaction
   return (
-        <div  className="flex gap-4 p-4 items-center justify-between ">
-          <div>
-            <p>{name}</p>
-            <p>{dateTime}</p>
-          </div>
-          <p>{amount}</p>
-          {/* <p>{description}</p> */}
-        </div>
+
+    <div className="card   w-96">
+  <div className="card-body flex justify-beteen">
+    <div>
+      <h2 className="card-title">{name}</h2>
+      <p>{dateTime}</p>
+      <p>{description}</p>
+    </div>
+    <div className=" justify-end">
+      <p>{amount}</p>
+    </div>
+  </div>
+</div>
+        // <div  className="flex gap-4 p-4 items-center justify-between ">
+        //   <div>
+        //     <p>{name}</p>
+        //     <p>{dateTime}</p>
+        //   </div>
+        //   <p>{amount}</p>
+        //   {/* <p>{description}</p> */}
+        // </div>
   )
 }
 
