e2e918efe38db8d264465e62918165b805ad54a5
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-10 03:04:08 +0200

    run async in useffect

--- src/components/dashboard/TransactionList.tsx
+++ src/components/dashboard/TransactionList.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+
+import { useEffect } from 'react'
+import { useFinanceStore } from '../../store/useFinanceStore'
+import { useAuthStore } from '../../store/useAuthStore';
 
 const TransactionList = () => {
+  const {grabTransactions, pending, transactions } = useFinanceStore()
+  const {userId} = useAuthStore();
+  useEffect(() => {
+    async function grab() {
+      await grabTransactions()
+    }
+    grab().then(() =>{
+      if(!pending)  console.log('transactions',transactions)
+    })
+  }, [userId])
+  
   return (
     <div>TransactionList</div>
   )
