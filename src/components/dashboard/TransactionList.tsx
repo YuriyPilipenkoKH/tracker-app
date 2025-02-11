890c34385530dd4cef7d011e76de005c46b9bba8
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-11 12:54:55 +0200

    applied pagination in transactions list

--- src/components/dashboard/TransactionList.tsx
+++ src/components/dashboard/TransactionList.tsx
@@ -1,31 +1,21 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useFinanceStore } from '../../store/useFinanceStore'
 import { useAuthStore } from '../../store/useAuthStore';
 import TransactionCard from './TransactionCard';
 
 const TransactionList = () => {
-  const {grabTransactions,  transactions } = useFinanceStore()
+  const {grabTransactions,  transactions, totalPages, currentPage } = useFinanceStore()
   const {userId} = useAuthStore();
+  const [page, setPage] = useState<number>(1);
   useEffect(() => {
-    async function grab() {
-      await grabTransactions()
-    }
-    grab().then(() =>{
-      // if(!pending)  console.log('transactions',transactions)
-    })
-  }, [userId])
+    grabTransactions({page:page, limit: 5});
+  }, [page, userId]);
 
-  // Sort transactions by `createdAt` in descending order
-  const sortedTransactions = [...transactions].sort((a, b) => {
-    const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
-    const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
 
-    return dateB - dateA; // Newest transactions first
-  });
   
   return (
     <div className='flex flex-col items-center gap-3 py-2'>
-      {sortedTransactions.map ((item, id) => (
+      {transactions.map ((item, id) => (
         <TransactionCard key={id} transaction={item}/> 
       ))}
     </div>
@@ -33,3 +23,12 @@ const TransactionList = () => {
 }
 
 export default TransactionList
+
+
+  // // Sort transactions by `createdAt` in descending order
+  // const sortedTransactions = [...transactions].sort((a, b) => {
+  //   const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
+  //   const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
+
+  //   return dateB - dateA; // Newest transactions first
+  // });
