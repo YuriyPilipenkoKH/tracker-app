c54663ea5685e632a1937b9a2ee7d12645776163
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-12 09:45:38 +0200

    rm extra

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -10,31 +10,3 @@ export const axios = axiosInstance.create({
   headers: { "Content-Type": "application/json" }
 })
 
-//
-
-export async function getTransactions() {
-  try {
-    const url = import.meta.env.HOST + '/api/transaction';
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error('Error fetching transactions:', error);
-    return [];
-  }
-}
-
-
-
-export async function getTotalBallance() {
- const transactions =  getTransactions().then(list => {
-
-     const totalBallance = list.reduce((acc: number, transaction: Transaction) => {
-        return acc + (transaction.ammount || 0);
-      }, 0);
-      return totalBallance
-})
- console.log(transactions)
- 
-}
-
