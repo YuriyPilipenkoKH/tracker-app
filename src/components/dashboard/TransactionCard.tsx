2095632dc6c16bd47a161473178f28d4edf61c60
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-24 21:53:45 +0200

    card refactor

--- src/components/dashboard/TransactionCard.tsx
+++ src/components/dashboard/TransactionCard.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Transaction } from "../../models/transaction"
 import { format } from "date-fns";
 import { cn } from "../../lib/cn";
-import { ChevronDown, SquarePen } from "lucide-react";
+import { ChevronDown, SquarePen, ZoomIn } from "lucide-react";
 
 interface TransactionCardProps{
 transaction: Transaction
@@ -26,14 +26,6 @@ const TransactionCard:React.FC<TransactionCardProps> = ({transaction}) => {
     <div 
       className="n12 w-full items-center  bg-base-300   p-5  border rounded-[12px] shadow md:w-96">
         <h2 className="name font-semibold h-6 overflow-hidden">{name}</h2>
-        <button
-          className='edit flex gap-2'
-          onClick={click}
-          >
-          <SquarePen
-          className={cn('transition-transform duration-1000 ease-in-out',
-          )}   />
-        </button>
         <p   className ={cn('amount font-bold text-xl', 
           amount < 0 
           ? "text-[orange]" : "text-green-500"
@@ -41,6 +33,12 @@ const TransactionCard:React.FC<TransactionCardProps> = ({transaction}) => {
         > {'$'}{' '}{Math.abs(amount)}
         </p>
         <div className="des font-light h-6 overflow-hidden">{description}</div>
+        <button
+          className='zoom flex gap-2'
+          onClick={click}
+          >
+          <ZoomIn />
+        </button>
         {createdAt && <p className="cat text-sm text-gray-500">{format(new Date(createdAt), "dd-MM-yyyy HH:mm")}
           </p>}
         <div className="total flex gap-1 text-sm text-gray-500">
@@ -54,3 +52,4 @@ const TransactionCard:React.FC<TransactionCardProps> = ({transaction}) => {
 }
 
 export default TransactionCard
+//ZoomIn
