a9234a32610e6cffd1abb677deb85307d63d324d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-24 16:55:11 +0200

    grid layout

--- src/components/dashboard/TransactionCard.tsx
+++ src/components/dashboard/TransactionCard.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { Transaction } from "../../models/transaction"
 import { format } from "date-fns";
 import { cn } from "../../lib/cn";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, SquarePen } from "lucide-react";
 
 interface TransactionCardProps{
 transaction: Transaction
@@ -24,35 +24,36 @@ const TransactionCard:React.FC<TransactionCardProps> = ({transaction}) => {
   return (
 
     <div 
-      className="n12  items-center  bg-base-300 w-64 p-4 border rounded-[12px] shadow md:w-96">
-      <div className="flex flex-col gap-2">
-        <div className="flex gap-1 justify-between">
-          <div className="font-semibold h-6 overflow-hidden">{name}</div>
-        { description &&  ( 
+      className="n12 w-full items-center  bg-base-300  p-4  border rounded-[12px] shadow md:w-96">
+
+
+          <h2 className="name font-semibold h-6 overflow-hidden">{name}</h2>
           <button
-          className='flex gap-2'
+          className='edit flex gap-2'
           onClick={click}
-            >
-          <ChevronDown
+          >
+          <SquarePen
           className={cn('transition-transform duration-1000 ease-in-out',
-            open ? 'rotate-180' : 'rotate-0'
           )}   />
           </button>
-        )}
-        </div>
-        {createdAt && <p className="text-sm text-gray-500">{format(new Date(createdAt), "dd-MM-yyyy HH:mm")}</p>}
-      </div>
-      <div className="flex flex-col items-end gap-2">
-        <p   className ={cn('font-bold text-xl', 
+
+
+        <p   className ={cn('amount font-bold text-xl', 
         amount < 0 
         ? "text-[orange]" : "text-green-500"
         )}
         > {'$'}{' '}{Math.abs(amount)}</p>
-        <div className="flex gap-1 text-sm text-gray-500">
+
+    {description &&  <div className="des font-light h-6 overflow-hidden">{description}</div>}
+
+        {createdAt && <p className="cat text-sm text-gray-500">{format(new Date(createdAt), "dd-MM-yyyy HH:mm")}</p>}
+
+
+        <div className="total flex gap-1 text-sm text-gray-500">
           <p>total:</p>
           <p>{total}</p>
         </div>
-      </div>
+  
     </div>
 
   )
