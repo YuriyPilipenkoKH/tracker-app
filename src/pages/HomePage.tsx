61fabb98f85eb3fa531442088c0d18cc5c86c276
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-25 19:06:34 +0200

    home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-
 const HomePage = () => {
+
   return (
-    <div className='flex'>
-      <div>HomePage</div>
-      <div>dom page</div>
+    <div className="grid transition-all ease-in-out duration-800">
+    <div className="flex flex-col items-center justify-center ">
+      <div className="w-full max-w-md space-y-8 text-xl py-4 px-8">
+      The Money Tracker App is a modern and efficient financial management tool built using React (Vite) and Node.js with TypeScript, Zustand for state management, and MongoDB for secure and scalable data storage
+
+        </div>
+        </div>
     </div>
   )
 }
