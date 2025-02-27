10b68a2fc3afad7b7e8f88f1fa1d717e4b992f10
Yuriy Pilipenko <yurik2061@gmail.com>
2025-02-27 11:55:59 +0200

    fhf applied

--- src/components/forms/EditTransactionForm.tsx
+++ src/components/forms/EditTransactionForm.tsx
@@ -1,8 +1,36 @@
 
 
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Transaction, transactionSchema } from '../../models/transaction'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const EditTransactionForm = () => {
+   const {
+      register, 
+      handleSubmit,
+      formState,
+      reset,
+      clearErrors,
+    } = useForm<Transaction>({
+      defaultValues: {  
+        name: '',
+        amount: undefined ,
+        description: '',
+         },
+          mode:'all',
+          resolver: zodResolver(transactionSchema), })
+      const {
+        errors,
+        isDirty,
+        isValid ,
+        isSubmitting,
+        isLoading 
+      } = formState
+
+    const onSubmit = async (data: Transaction) => {
+      
+    }
   return (
     <div>EditTransactionForm</div>
   )
